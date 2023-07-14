cf15d54dddff46cdce6f5dc3aefdb324f1496fa0
Lucas Lousada <lucas_lousada@outlook.com>
2023-07-14 15:23:11 -0300

    finalize part of the structure and style of the products page
    product type guides and table began to be developed

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+import { Root as TabsRoot, Content as TabsContent } from '@radix-ui/react-tabs';
 import { Menu } from '../../components/Menu';
 import { Breadcrumbs } from '../../components/Breadcrumbs';
 
 import { CaretRight } from 'phosphor-react';
 
-import { Container, Main } from './styles';
+import { Container, Main, Table, TabsList, TabsTrigger } from './styles';
+
+type TypesOfProducts = 'all' | 'simple';
 
 export function Products() {
+  const [displayedProductTypeTab, setDisplayedProductTypeTab] =
+    useState<TypesOfProducts>('all');
+
   return (
     <Container>
       <Menu />
@@ -17,6 +24,40 @@ export function Products() {
           </li>
           <li>Produtos</li>
         </Breadcrumbs>
+        <h1>Produtos</h1>
+        <TabsRoot
+          value={displayedProductTypeTab}
+          onValueChange={setDisplayedProductTypeTab as (value: string) => void}
+        >
+          <TabsList>
+            <TabsTrigger value="all">
+              todos <p>127</p>
+            </TabsTrigger>
+            <TabsTrigger value="simple">
+              simples <p>110</p>
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value={displayedProductTypeTab}>
+            <Table>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>SKU</th>
+                  <th>Preço</th>
+                  <th>Estoque</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Camiseta Cinza</td>
+                  <td>32409922</td>
+                  <td>R$ 50</td>
+                  <td>11</td>
+                </tr>
+              </tbody>
+            </Table>
+          </TabsContent>
+        </TabsRoot>
       </Main>
     </Container>
   );
--- src/pages/Products/styles.ts
+++ src/pages/Products/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { List, Trigger } from '@radix-ui/react-tabs';
 
 export const Container = styled.div`
   display: flex;
@@ -7,4 +8,81 @@ export const Container = styled.div`
 export const Main = styled.main`
   width: 100%;
   padding: 40px;
+
+  h1 {
+    font-size: 20px;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.color_900};
+  }
+`;
+
+export const TabsList = styled(List)`
+  display: flex;
+  gap: 0 16px;
+  margin-top: 40px;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.transparent_color_100};
+`;
+
+export const TabsTrigger = styled(Trigger)`
+  position: relative;
+  padding: 10px 20px;
+  background-color: transparent;
+  text-align: center;
+  font-size: 15px;
+  color: ${({ theme }) => theme.colors.color_800};
+  border: none;
+
+  &[data-state='active'] {
+    color: ${({ theme }) => theme.colors.color_900};
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: -1px;
+      right: 0;
+      left: 0;
+      height: 2px;
+      background-color: ${({ theme }) => theme.colors.color_900};
+    }
+  }
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  margin-top: 20px;
+  border-spacing: 0;
+  border-radius: 5px;
+  border: 1px solid ${({ theme }) => theme.colors.transparent_color_100};
+  text-align: left;
+
+  th {
+    padding: 17.5px 0;
+    font-size: 14px;
+    font-weight: 500;
+    color: ${({ theme }) => theme.colors.color_800};
+    border-bottom: 1px solid
+      ${({ theme }) => theme.colors.transparent_color_100};
+  }
+
+  td {
+    padding: 16.5px 0;
+    font-size: 15px;
+    color: ${({ theme }) => theme.colors.color_900};
+    border-bottom: 1px solid
+      ${({ theme }) => theme.colors.transparent_color_100};
+  }
+
+  tr td:first-of-type,
+  tr th:first-of-type {
+    padding-left: 20px;
+  }
+
+  tr td:last-of-type,
+  tr th:last-of-type {
+    padding-right: 20px;
+  }
+
+  tr:last-of-type td {
+    border: none;
+  }
 `;
